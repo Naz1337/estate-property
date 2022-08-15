87987ebb183fbd35c049094322bcab4fd164202a
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2022-08-15 13:01:38 +0800

    chapter7

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,8 @@
     'installable': True,
     'application': True,
     "data": [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
     ]
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,9 +9,9 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda _: fields.Date.today() + relativedelta(months=2))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
+    selling_price = fields.Float(readonly =True, copy=False)
     bedrooms = fields.Integer()
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -19,3 +21,11 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         selection=[("north", "North"), ("south", "South"),
                    ("east", "East"), ("west", "West")])
+    state = fields.Selection(
+        selection=[("new", "New"), ("offer_received", "Offer Received"),
+                   ("offer_accepted", "Offer Accepted"), ("sold", "Sold"),
+                   ("canceled", "Canceled")],
+        default="new")
+    
+    active = fields.Boolean(default=True, string="isActive?")
+    
